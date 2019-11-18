902506b0e97342d16c510e32527b0c6b847353d9
Kurt Bauer <kurtbauer55@protonmail.com>
2019-11-17 22:47:17 -0500

    querying DOM through this reference back to HTML

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import '../assets/css/style.css';
 
+//===> QUERYING DOM THROUGH THIS REFERENCE BACK TO HTML
 const app = document.getElementById('app');
 app.innerHTML = '<h1>JavaScript Basics</h1>';
