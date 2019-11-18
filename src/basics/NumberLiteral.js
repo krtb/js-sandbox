2bb3f9b4c859b2a5d98998dd0a5c11dbd6759221
Kurt Bauer <kurtbauer55@protonmail.com>
2019-11-18 12:02:39 -0500

    remove duplicate file

--- src/basics/NumberLiteral.js
+++
@@ -1,5 +0,0 @@
-// NUMBER LITERAL / FUNCTION / CONSTRUCTOR SYNTAX
-
-// FAVORITE (NUMBER LITERAL syntax)
-const literalNumber = 99;
-console.log(literalNumber) // BLUE: NUMBER / BLACK: string, or other
