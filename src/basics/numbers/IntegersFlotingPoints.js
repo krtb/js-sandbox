2b711cdb7220316df6790ce4908d3ff405a41c80
Kurt Bauer <kurtbauer55@protonmail.com>
2019-11-18 12:01:34 -0500

    remove rounding errors by dividing by 100

--- src/basics/numbers/IntegersFlotingPoints.js
+++ src/basics/numbers/IntegersFlotingPoints.js
@@ -12,3 +12,9 @@ console.log(0.1 * 0.2); // 0.300000000004 ROUNDING ERROR
 const price = 9.33;
 const quantity = 3;
 console.log(price * quantity); // 27.9900000000002 ROUNDING ERROR
+
+// EX 2
+const anotherPrice = 9.33;
+const anotherQuantity = 3;
+
+console.log((anotherPrice * anotherQuantity) / 100) // 27.99
