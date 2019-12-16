d604fd36aa8e8654d7e0d572440d4256a12bdfb6
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-16 10:45:28 -0500

    add two functions

---
+++ modern-js/functions/functions101.js
@@ -0,0 +1,15 @@
+// function - input, code, output
+
+let greetUser = function () {
+    console.log('welcome!');
+}
+
+greetUser()
+greetUser()
+greetUser()
+
+let squareThis = function (num) {
+    console.log(num);
+}
+
+squareThis(4)
