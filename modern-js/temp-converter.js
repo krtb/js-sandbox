c0ff0deb538c60e4980cbc5ee6ef8d326365b085
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-15 21:31:47 -0500

    create celsius converter function

---
+++ modern-js/temp-converter.js
@@ -0,0 +1,10 @@
+let fahrenheit = 50
+
+// calc celsius and store in celsius variable
+// print value
+function celsiusConverter(params) {    
+    let convertedTemp = (params - 32) * 5 / 9
+    console.log(convertedTemp)
+}
+
+celsiusConverter(fahrenheit)
