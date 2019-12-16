d2468ea1c89c90e1418c3f87a624b8c66bfd386f
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-16 10:52:55 -0500

    undefined for missing return value

--- modern-js/functions/undefinedNull.js
+++ modern-js/functions/undefinedNull.js
@@ -1,4 +1,4 @@
-//undefined for variable
+//1) undefined for variable
 let name
 
 if (name === undefined) {
@@ -7,9 +7,16 @@ if (name === undefined) {
     console.log(name);
 }
 
-// Undefined for function arguments that aren't provided
+// 2) Undefined for function arguments that aren't provided
 
 let square = function (num) {
-    console.log(num);
+    console.log(num); //undefined
+    // haven't used return in function
 }
+
+// undefined but not return in function
+// try to do something with return value but
+// nothing is being returned
+let result = square()
+console.log(result); // undefined
 
