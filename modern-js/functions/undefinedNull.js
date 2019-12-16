124a2c90d3891677439d2a3b1641b21874d1e433
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-16 10:50:19 -0500

    undefined for vars and func args not provided

---
+++ modern-js/functions/undefinedNull.js
@@ -0,0 +1,15 @@
+//undefined for variable
+let name
+
+if (name === undefined) {
+    console.log("provide a name");
+} else {
+    console.log(name);
+}
+
+// Undefined for function arguments that aren't provided
+
+let square = function (num) {
+    console.log(num);
+}
+
