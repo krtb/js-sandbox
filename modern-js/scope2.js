8f4458572be3a37ef5be9380e0413d44287fd100
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-16 10:28:29 -0500

    add leaked variable example

--- modern-js/scope2.js
+++ modern-js/scope2.js
@@ -5,17 +5,20 @@
         //local
     //local   
 
-let name = "me"
+// let name = "me"
 
 if (true) { 
     // variable shadowing
     // local variable value (shadowing) over parent above
-    let name = "not me"  
+    // let name = "not me"  
 
     if (true) {
         // changes name var to not found,
         // stops at 'not me', changes to 'not found'
         // doesn't change the global scope variable 
+
+        //leaked variable, wasn't explicitly defined, 
+        // JS creates a new global variable 
         name = "not found"
 
         // scope tree traversed upwards to find first definition of variable
