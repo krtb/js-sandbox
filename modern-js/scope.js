fc65182b97b28be3a2e92c5d48b88ec51733af7e
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-16 09:57:25 -0500

    add inline comment on parent scope

--- modern-js/scope.js
+++ modern-js/scope.js
@@ -1,6 +1,13 @@
 // LEXICAL SCOPE (static scope) 
-// Global Scope - defined outside of code block
+// Global Scope - defined outside of code block 
 // Local Scope - defined inside of code block
+
+// in scope, can access vars defined inside,
+// or any parent scope  
+
+
+// global scope = varOne
+// local scope = varTwo
 
 let varOne = "varOne"
 
