794573c60febb72b231e06ef8a9ee1356e087346
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-16 11:05:23 -0500

    provide default values to function params

--- modern-js/functions/args.js
+++ modern-js/functions/args.js
@@ -5,3 +5,13 @@ let add = function (a,b,c) {
 
 let result = add(10,1,5)
 console.log(result);
+
+// DEFAULT ARGUMENTS
+let getScoreText = function (name, score = 0) {
+    console.log(name);
+    console.log(score);
+}
+
+// getScoreText() => by default if no argument value, undefined is result
+// provide default value, which WAS undefined, not provided in function
+getScoreText()
